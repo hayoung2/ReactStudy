0396dd33be88a5fdf8bda9bc828b9abab184d8de
hayoung2 <gkdud1308@naver.com>
2020-12-06 03:47:07 +0900

    ENH: font 추가 및 Route 설정

--- workerProject/front/src/App.js
+++ workerProject/front/src/App.js
@@ -1,17 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 import Start from './components/Start';
 import Main from './components/Main';
-import { Route } from 'react-router-dom';
 import Community from './components/Community';
+import Post from './components/Post';
 
-function App() {
-  return (
-    <div>
-      <Route path='/' component={Start} exact={true} />
-      <Route path='/home' component={Main} />
-    </div>
-  );
+
+class App extends React.Component {
+
+
+
+  render() {
+    return (
+      <div>
+        <Route path='/' component={Start} exact={true} />
+        <Route path='/home' component={Main} />
+        <Route path='/community' component={Community} />
+        <Route path='/post' component={Post} />
+      </div>
+    )
+  }
 }
 
 export default App;
