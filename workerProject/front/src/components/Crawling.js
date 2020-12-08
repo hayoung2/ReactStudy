29e843d2adcdc718fb29d3e21b182b6516a07d72
hayoung2 <gkdud1308@naver.com>
2020-12-09 03:27:24 +0900

    ENH: Main 크롤링 부분 구현 완료
    css 부분 수정 필요

--- workerProject/front/src/components/Crawling.js
+++ workerProject/front/src/components/Crawling.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TweenMax, Power3 } from 'gsap';
+
 
 
 class Crawling extends React.Component {
@@ -8,23 +8,27 @@ class Crawling extends React.Component {
     super(props);
     this.content = null;
     this.state = {
-      data: "",
-      datas: ""
+      coronaData: "",
+      weatherData: "",
+      dustData: ""
     }
 
   }
 
 
   componentDidMount() {
-    this.callApi().then(res => this.setState({ data: res })).catch(err => console.log(err));
+    this.callApi().then(res => this.setState({ coronaData: res })).catch(err => console.log(err));
   }
 
   callApi = async () => {
     const response = await fetch('/crawling/corona');
-    const response1 = await fetch('/crawling/data');
+    const response1 = await fetch('/crawling/weather');
+    const dust = await fetch('/crawling/dust');
     const body = await response.json();
     const body1 = await response1.json();
-    this.setState({ datas: body1 });
+    const dustBody = await dust.text()
+    this.setState({ weatherData: body1 });
+    this.setState({ dustData: dustBody.substr(35, 2) });
     return body;
   }
 
@@ -35,45 +39,35 @@ class Crawling extends React.Component {
         <h1 style={styles.bold}>직장인 필수</h1>
         <p style={styles.light}>" 날씨, 확진자, 미세먼지 3가지 모두 확인! "</p>
         <div className="info_wrap">
-          <img src="https://cdn.pixabay.com/photo/2018/10/13/17/53/chasing-3744753_960_720.jpg" className="info_image" />
+          <img src="https://cdn.pixabay.com/photo/2018/10/13/17/53/chasing-3744753_960_720.jpg" className="info_image" alt="dust" />
           <h2 className="info_title">미세먼지</h2>
-          {
-            this.state.datas ? this.state.datas.map(e => {
-              return (
-                <>
-                  <p className="info_content">{e.temperature ? e.temperature : ''}</p>
-                </>
-              )
-
-            }) : <div>없음</div>
-
-          }
+          <p className="info-content">{this.state.dustData ? this.state.dustData.replaceAll('\t', '') : ''}</p>
         </div>
         <div className="info_wrap">
-          <img src="https://cdn.pixabay.com/photo/2018/10/13/17/53/chasing-3744753_960_720.jpg" className="info_image" />
+          <img src="https://cdn.pixabay.com/photo/2018/10/13/17/53/chasing-3744753_960_720.jpg" className="info_image" alt="corona" />
           <h2 className="info_title">코로나</h2>
           {
-            this.state.data ? this.state.data.map(e => {
+            this.state.coronaData ? this.state.coronaData.map(e => {
               return (
                 <>
-                  <p className="info_content">{e.text === "확진환자" ? e.num : ''}</p>
+                  <p className="info-content">{e.text === "확진환자" ? e.num.substr(21, 5).replaceAll(')', '') : ''}</p>
                 </>
               )
-            }) : <div>없음</div>
+            }) : <div className="info-content">없음</div>
 
           }
         </div>
         <div className="info_wrap">
-          <img src="https://cdn.pixabay.com/photo/2018/10/13/17/53/chasing-3744753_960_720.jpg" className="info_image" />
+          <img src="https://cdn.pixabay.com/photo/2018/10/13/17/53/chasing-3744753_960_720.jpg" className="info_image" alt="weather" />
           <h2 className="info_title">날씨</h2>
           {
-            this.state.datas ? this.state.datas.map(e => {
+            this.state.weatherData ? this.state.weatherData.map(e => {
               return (
                 <>
-                  <p className="info_content">{e.temperature ? e.temperature : ''}</p>
+                  <p className="info-content">{e.temperature ? e.temperature : ''}</p>
                 </>
               )
-            }) : <div>없음</div>
+            }) : <div className="info-content">없음</div>
 
           }
         </div>
@@ -90,7 +84,7 @@ const styles = {
   light: {
     fontFamily: 'Jeju Myeongjo',
     marginLeft: '40vw',
-    fontSize: 20,
+    fontSize: 24,
     marginTop: '40px'
   }
 }
