38d48615819c0c00267fd707c884e3c766fd8351
hayoung2 <gkdud1308@naver.com>
2020-12-12 05:00:56 +0900

    ENH: Header 애니메이션 추가 및 링크 연결

--- workerProject/front/src/components/Header.js
+++ workerProject/front/src/components/Header.js
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import blog from '../images/blog.png';
 import github from '../images/github.png';
+import { Link } from 'react-router-dom';
 
 const loaderVariants = {
   animationOne: {
@@ -21,24 +22,35 @@ const loaderVariants = {
 }
 
 
+
 function Header() {
   return (
     <div style={styles.menu}>
-      <div style={styles.loader_wrap}>
-        <motion.div className='loader'
-          variants={loaderVariants}
-          animate="animationOne">
-        </motion.div>
-      </div>
+      <Link to="/home">
+        <div style={styles.loader_wrap}>
+          <motion.div className='loader'
+            variants={loaderVariants}
+            animate="animationOne">
+          </motion.div>
+        </div>
+      </Link>
       <div style={styles.menu_list_wrap}>
+
+
         <a href="https://github.com/hayoung2" >
-          <img className="header-logo" src={blog} />
+          <img className="header-logo" src={blog} alt="blog-logo" />
         </a>
+
         <a href="https://github.com/hayoung2" >
-          <img className="header-logo" src={github} />
+          <img className="header-logo" src={github} alt="github-logo" />
         </a>
-        <p className="menu-list-item">Community</p>
-        <p className="menu-list-item">ForeCast</p>
+
+        <Link to="/community" className="link-to-style">
+          <p className="menu-list-item">Community</p>
+        </Link>
+        <a href="http://localhost:3000/home#crawling">
+          <p className="menu-list-item">ForeCast</p>
+        </a>
         <p className="menu-list-item">About</p>
 
       </div>
@@ -49,7 +61,8 @@ const styles = {
   menu: {
     width: '100%',
     height: '10vh',
-    backgroundColor: 'white'
+    color: 'black',
+    opacity: '0.8',
   },
   menu_list_wrap: {
     marginRight: '9vw',
